8dda0c9deea7081d966e2f8683831c90c490b1a9
Iraline <iraline@hotmail.com>
2022-01-17 17:29:37 -0300

    feat: Create a test for a treat error when the file doesnt exist

--- spec/main_spec.rb
+++ spec/main_spec.rb
@@ -1,8 +1,7 @@
 require 'main'
 
 describe 'Main' do
-    it 'should read the first line of the file and print into screen' do
-       
+    it 'should read the first line of the file and print into screen' do   
         @content = "Test 1,2,3"
 
         arq = File.new("test.log","w")
@@ -12,5 +11,12 @@ describe 'Main' do
         main = Main.new(arq)
 
         expect(main.getFirstLine).to eq(@content)
+    end
+
+    it 'should treat error when file doesn`t exist' do
+        arq = "teste"
+        main = Main.new(arq)
+
+        expect(main.file).to eq(nil)
     end
 end 
