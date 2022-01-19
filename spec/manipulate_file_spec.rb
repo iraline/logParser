c0d302942522643c80677d6bdd4df6bd11d6760b
Iraline <iraline@hotmail.com>
2022-01-19 10:49:56 -0300

    Create a test to json function

--- spec/manipulate_file_spec.rb
+++ spec/manipulate_file_spec.rb
@@ -20,3 +20,44 @@ describe '#get_first_line' do
     expect(main.file).to eq(nil)
   end
 end 
+
+describe '#qty_lines' do
+  it 'read number of lines of a file' do   
+    @content = "Test 1,2,3"
+
+    arq = File.new("test.log","w")
+    arq.puts @content
+    arq.close
+
+    main = ManipulateFile.new(arq)
+
+    expect(main.qty_lines).to be > 0
+  end
+end 
+
+describe '#mount_json' do
+  it 'create a json object' do   
+    
+    @content = "Test 1,2,3"
+
+    arq = File.new("test.log","w")
+    arq.puts @content
+    arq.close
+
+    main = ManipulateFile.new(arq)
+   
+    obj = { main.name => { "lines" => main.qty_lines} }
+
+
+    expect(main.mount).to eq(@content)
+  end
+end 
+
+context "when user asks number of lines" do
+  it "print a JSON object showing the number" do
+    result = LogParser.new.line_count
+    JSONobj = {"games.log":{"lines":5306}}.to_json
+    expect(result).to eq(JSONobj)
+  end
+end
+
