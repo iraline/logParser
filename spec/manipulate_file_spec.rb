d2da0370258a65eb12625a71eba95f6586856ef1
Iraline <iraline@hotmail.com>
2022-02-01 14:12:21 -0300

    Refactor

--- spec/manipulate_file_spec.rb
+++ spec/manipulate_file_spec.rb
@@ -5,9 +5,8 @@ require 'manipulate_file'
 describe ManipulateFile do
   describe '#show_first_line' do
     it 'read the first line of the file' do
-     
-      main = ManipulateFile.new("test.txt")
-     
+      main = ManipulateFile.new('test.txt')
+
       expect(main.show_first_line).to eq('Test 1,2,3')
     end
 
@@ -20,23 +19,20 @@ describe ManipulateFile do
 
   describe '#mount_object' do
     it 'create a response object' do
-
       main = ManipulateFile.new('test.txt')
 
-      obj = { 'test.txt' => { 'lines' => 6, 'players' => ['Test1','Test2','Test3'] } }
+      obj = { 'test.txt' => { 'lines' => 6, 'players' => %w[Test1 Test2 Test3] , 'kills'=> {'Test1'=>1 , 'Test2'=>1},"total_kills"=>2} }
 
       expect(main.mount_object).to eq(obj)
     end
   end
-end 
 
-describe '#get_players_kills' do
-  it 'get quantity of kills from each player and de sum of all' do   
-    @content = [{"Test1"=> 1 , "Test2"=> 1},2]
-
-    main = ManipulateFile.new("test.txt")
-
-    expect(main.get_players_kills).to eq(@content)
+  describe '#show_players_kills' do
+    it 'show quantity of kills from each player and de sum of all' do
+      main = ManipulateFile.new('test.txt')
+  
+      expect(main.show_players_kills).to eq([{ 'Test1' => 1, 'Test2' => 1 }, 2])
+    end
   end
-end 
 
+end
