1dd80678d4d8f544156c89a53c896cf43f4e0009
Iraline <iraline@hotmail.com>
2022-02-01 14:11:47 -0300

    Refactor

--- lib/main.rb
+++ lib/main.rb
@@ -2,13 +2,6 @@
 
 require './manipulate_file'
 
-<<<<<<< HEAD
-kills = file.get_players_kills
-
-obj = { file.name => { "lines" => file.qty_lines, "players" => file.get_players_name, "kills" => kills[0], "total_kills" => kills[1]} }
-
-=======
 file = ManipulateFile.new('../games.log')
->>>>>>> 6c22aeb185c6034bcd611ed6b8badf257adc366b
 
 puts JSON.pretty_generate(file.mount_object)
--- lib/manipulate_file.rb
+++ lib/manipulate_file.rb
@@ -1,4 +1,5 @@
 # frozen_string_literal: true
+
 require 'json'
 
 # Class to help manipulate a file
@@ -16,30 +17,13 @@ class ManipulateFile
     @file[0]
   end
 
-  def mount_object
-    obj = { @name => { 'lines' => @file.length, 'players' => show_players_name } }
-  end
-
-  private 
- 
-  def show_players_name
-    players = []
-    @file.each do |line|
-      next unless line.include?('ClientUserinfoChanged:')
-
-      player = line.split('\\')
-      players.append(player[1]) unless players.include?(player[1])
-    end
-    players
-  end
-
-  def get_players_kills
+  def show_players_kills
     kills = Hash[]
     x = 0
     for element in self.file do
       if element.include?('Kill:')
-        kill = element.split(' ') 
-        if !kill[5].include?('<world>')
+        kill = element.split(" ") 
+        if !kill[5].include?("<world>")
           x +=1
           if kills.include?(kill[5])
             kills[kill[5]] += 1
@@ -50,6 +34,27 @@ class ManipulateFile
       end
     end
     [kills,x]
+  end
+
+  def mount_object
+    kills = show_players_kills
+    { @name => { 'lines' => @file.length, 'players' => show_players_name, 'kills' => kills[0],
+                 'total_kills' => kills[1] } }
+  end
+
+  private
+
+  def show_players_name
+    players = []
+    @file.each do |element|
+      if element.include?('ClientUserinfoChanged:')
+        player = element.split('\\')
+        if not players.include?(player[1])
+          players.append(player[1])
+        end
+      end
+    end
+    return players
   end
 
 end
